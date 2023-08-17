0e6ee1c2d0846fb3fda9b92a7de306826904e71c
Badhon100 <badhonx102@gmail.com>
2023-08-18 00:45:14 +0600

    selecting and previewing selescted video

--- lib/view/screens/confirm_screen.dart
+++ lib/view/screens/confirm_screen.dart
@@ -1,12 +1,107 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ConfirmScreen extends StatelessWidget {
-  const ConfirmScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/view/widgets/text_input_field.dart';
+import 'package:video_player/video_player.dart';
+
+class ConfirmScreen extends StatefulWidget {
+  
+  final File videoFile;
+  final String videoPath;
+  const ConfirmScreen({super.key, required this.videoFile, required this.videoPath});
+  @override
+  State<ConfirmScreen> createState() => _ConfirmScreenState();
+}
+
+class _ConfirmScreenState extends State<ConfirmScreen> {
+  late VideoPlayerController controller;
+
+  TextEditingController songController = TextEditingController();
+  TextEditingController captionController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      controller = VideoPlayerController.file(widget.videoFile);
+    });
+    controller.initialize();
+    controller.play();
+    controller.setVolume(1);
+    controller.setLooping(true);
+  }  
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height/1.5,
+                child: VideoPlayer(controller),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: MediaQuery.of(context).size.width - 20,
+                      child: TextInputField(
+                        controller: songController, 
+                        labelText: 'Song Name', 
+                        icon: Icons.music_note,
+                        isObscured: false
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: MediaQuery.of(context).size.width - 20,
+                      child: TextInputField(
+                        controller: captionController, 
+                        labelText: 'Caption', 
+                        icon: Icons.closed_caption,
+                        isObscured: false
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    ElevatedButton(
+                      onPressed: (){}, 
+                      child: const Text(
+                        "Share",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
