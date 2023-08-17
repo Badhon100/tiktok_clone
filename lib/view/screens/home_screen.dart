6539da7becb47bfca60f1aa084af8dfd0fd5e5eb
Badhon100 <badhonx102@gmail.com>
2023-08-17 17:33:13 +0600

    home screen bottom navigation bar done

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/const.dart';
+import 'package:tiktok_clone/view/widgets/custom_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home"),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value){
+          setState(() {
+            pageIndex = value;
+          });
+        },
+        backgroundColor: backgroundColor,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: buttonColor,
+        unselectedItemColor: Colors.white,
+        currentIndex: pageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 30,),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 30,),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: CustomIcon(),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 30,),
+            label: "Message",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30,),
+            label: "Profile",
+          ),
+        ]
       ),
+      body: pages[pageIndex],
     );
   }
 }
