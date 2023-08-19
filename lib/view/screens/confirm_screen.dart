3d1267e094d2eee6ae3980bcc169a71aca58e3da
Badhon100 <badhonx102@gmail.com>
2023-08-19 23:49:41 +0600

    null error while uploading video

--- lib/view/screens/confirm_screen.dart
+++ lib/view/screens/confirm_screen.dart
@@ -1,27 +1,33 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/controller/upload_video_controller.dart';
 import 'package:tiktok_clone/view/widgets/text_input_field.dart';
 import 'package:video_player/video_player.dart';
 
 class ConfirmScreen extends StatefulWidget {
-  
   final File videoFile;
   final String videoPath;
-  const ConfirmScreen({super.key, required this.videoFile, required this.videoPath});
+  const ConfirmScreen({
+    Key? key,
+    required this.videoFile,
+    required this.videoPath,
+  }) : super(key: key);
+
   @override
   State<ConfirmScreen> createState() => _ConfirmScreenState();
 }
 
 class _ConfirmScreenState extends State<ConfirmScreen> {
   late VideoPlayerController controller;
+  TextEditingController _songController = TextEditingController();
+  TextEditingController _captionController = TextEditingController();
 
-  TextEditingController songController = TextEditingController();
-  TextEditingController captionController = TextEditingController();
+  UploadVideoController uploadVideoController =
+      Get.put(UploadVideoController());
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     setState(() {
       controller = VideoPlayerController.file(widget.videoFile);
@@ -30,11 +36,10 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
     controller.play();
     controller.setVolume(1);
     controller.setLooping(true);
-  }  
+  }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     controller.dispose();
   }
@@ -42,64 +47,67 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height/1.5,
-                child: VideoPlayer(controller),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      width: MediaQuery.of(context).size.width - 20,
-                      child: TextInputField(
-                        controller: songController, 
-                        labelText: 'Song Name', 
-                        icon: Icons.music_note,
-                        isObscured: false
-                      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.5,
+              child: VideoPlayer(controller),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: TextInputField(
+                      controller: _songController,
+                      labelText: 'Song Name',
+                      icon: Icons.music_note,
+                      isObscured: false,
                     ),
-                    const SizedBox(height: 10,),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      width: MediaQuery.of(context).size.width - 20,
-                      child: TextInputField(
-                        controller: captionController, 
-                        labelText: 'Caption', 
-                        icon: Icons.closed_caption,
-                        isObscured: false
-                      ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: TextInputField(
+                      controller: _captionController,
+                      labelText: 'Caption',
+                      icon: Icons.closed_caption,
+                      isObscured: false,
                     ),
-                    const SizedBox(height: 10,),
-                    ElevatedButton(
-                      onPressed: (){}, 
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                      onPressed: () => uploadVideoController.uploadVideo(
+                          _songController.text,
+                          _captionController.text,
+                          widget.videoPath),
                       child: const Text(
-                        "Share",
+                        'Share!',
                         style: TextStyle(
+                          fontSize: 20,
                           color: Colors.white,
-                          fontSize: 20
                         ),
-                      )
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                      ))
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
